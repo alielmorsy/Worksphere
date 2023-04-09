826fbfc0dadd42e919836b394d33a07a7abe5a34
mo-ghattas <124355286+mo-ghattas@users.noreply.github.com>
2023-04-09 03:06:54 +0200

    .

--- management/models.py
+++ management/models.py
@@ -40,5 +40,4 @@ class Task(models.Model):
     additional_fields = ArrayField(model_container=TaskCustomFields, null=True, blank=True)
     state = models.IntegerField(choices=State.choices, default=State.OPEN)
     subTasks = ArrayReferenceField(to="self", null=True, blank=True)  # Not All Tasks has sub-tasks.
-    taskState = models.IntegerField(choices=ChannelType.choices, default=ChannelType.CHAT)
     manager = DjongoManager()
